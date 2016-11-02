285c308734514a3f04be1c42672351b8d86ad450
Stephanholly <sholly@usc.edu>
2016-11-02 11:01:12 -0700

    first commit

---
+++ netflixbinge.swift
@@ -0,0 +1,18 @@
+// We've all binged an entire season of House of Cards, Orange is the New Black, or The Office on Netflix in one weekend. Don't try to deny it. This time, you're about to embark on a 10 hour Netflix binge and want to create a program that counts down the number of hours you have watched Netflix, and the number of hours you have left of your binge, until its over!
+
+// Your should see the following:
+
+// 0 hours of Netflix down, 10 to go.
+// 1 hours of Netflix down, 9 to go.
+// 2 hours of Netflix down, 8 to go.
+// 3 hours of Netflix down, 7 to go.
+// 4 hours of Netflix down, 6 to go.
+// 5 hours of Netflix down, 5 to go.
+// 6 hours of Netflix down, 4 to go.
+// 7 hours of Netflix down, 3 to go.
+// 8 hours of Netflix down, 2 to go.
+// 9 hours of Netflix down, 1 to go.
+// 10 hours of Netflix down, 0 to go.
+//=> "Netflix binge complete"
+
+// Enter code below:
